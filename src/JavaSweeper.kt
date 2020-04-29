ef9d7fbdb882e7e80d524fb780fad5c0907b0472
Insafprog <insaf@gmx.ru>
2020-04-29 15:01:51 +0400

    initial commit

--- src/JavaSweeper.kt
+++ src/JavaSweeper.kt
@@ -1,8 +1,10 @@
-class JavaSweeper {
+import javax.swing.JFrame
+
+class JavaSweeper: JFrame() {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            
+
         }
     }
 }
