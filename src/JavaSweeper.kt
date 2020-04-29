e6af8e847cacc8a9ec5b6cf10895438f473dce64
Insafprog <insaf@gmx.ru>
2020-04-29 15:28:40 +0400

    images add

--- src/JavaSweeper.kt
+++ src/JavaSweeper.kt
@@ -1,10 +1,36 @@
+import java.awt.Dimension
 import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.WindowConstants
 
 class JavaSweeper: JFrame() {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
 
-        }
+    private lateinit var panel: JPanel
+    private val cols = 15
+    private val rows = 1
+    private val imageSize = 50
+
+    init {
+        initPanel()
+        initFrame()
     }
+
+    private fun initPanel() {
+        panel = JPanel()
+        panel.preferredSize = Dimension(cols * imageSize, rows * imageSize)
+        add(panel)
+    }
+
+    private fun initFrame() {
+        pack()
+        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+        title = "Java Sweeper"
+        setLocationRelativeTo(null)
+        isResizable = false
+        isVisible = true
+    }
+}
+
+fun main() {
+    JavaSweeper()
 }
